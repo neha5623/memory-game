8b42e13b295d4c6c4dd9eb40009034c0bf2d6a18
neha5623 <neha.naseer610@gmail.com>
2025-06-01 01:44:02 +0530

    some polishing

--- src/components/block.js
+++ src/components/block.js
@@ -1,8 +1,10 @@
 import React from 'react';
 export default function Block(){
     return (
-        <div style={{backgroundColor:'#ccd5bc',width:'100px',height:'100px',borderRadius:'10px',border:'5px solid black'}}>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <div style={{backgroundColor:'#ccd5bc',width:'125px',height:'125px',borderRadius:'10px',border:'5px solid blue'}}>
 
+        </div>
         </div>
     )
 
